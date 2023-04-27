e36f5004f1abe494ce6d0b342e5dd8624d763e3e
Tushar Jain <ishaanjainlock10@gmail.com>
2023-04-28 00:11:01 +0530

    lol

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -5,14 +5,14 @@ const NotesContext = React.createContext()
 const notesReducer =  (state, action) => {
     switch (action.type) {
         case "ADD_NOTE":
-            return [...state, {title:`post #${state.length + 1}`, description:"desc"}]
+            return [...state, {title:`post #${state.length + 1}`, description:"desc",id : Math.floor(Math.random()*99999)}]
         default:
             return state
     }
 }
 
 export const NotesProvider = ( {children} ) => {
-    const [Notes, dispatch] = useReducer(notesReducer, [{title:"balls", description:"balls2"}])
+    const [Notes, dispatch] = useReducer(notesReducer, [{title:"Note title", description:"Testing"}])
 
     const addNotes = () => {
         dispatch({type:"ADD_NOTE"})
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,44 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import { View,Text,Button, TextInput } from "react-native"
+import { StyleSheet } from "react-native";
 import NotesContext from "../context/NotesContext";
 import { FlatList } from "react-native-gesture-handler";
-
+import { Feather } from "@expo/vector-icons";
 const IndexScreen = () => {
     const value = useContext(NotesContext)
     return (
         <View>
-            <Text>
-                HomeScreen
-            </Text>
+            <Button title = "add note" onPress={() => value.addNotes()} />
             <FlatList
                 data={value.Notes}
-                renderItem={({item}) => <Text>{item.title} is the title, {item.description} is content</Text>}
+                renderItem={({item}) => (
+                    <View style={styles.row}>
+                        <Text style = {styles.title}>{item.title} is the title, {item.description} is content</Text>
+                        <Feather style={styles.icon} name = "trash" />
+                    </View>
+                )}
                 keyExtractor={(item) => item.title}
             />
-            <Button title = "add note" onPress={() => value.addNotes()} />
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    row:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        paddingVertical:20,
+        paddingHorizontal:10,
+        borderTopWidth:1,
+        borderBottomWidth:1,
+        borderColor:"gray"
+    },
+    title: {
+        fontSize:16
+    },
+    icon: {
+        fontSize:24
+    }
+})
 
 export default IndexScreen;
